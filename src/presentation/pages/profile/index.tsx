0bca72bd6dbc39a70f571a351e5d10b00ecc56eb
Jean <jean.carlosmolossi@gmail.com>
2021-12-12 00:08:28 -0300

    feat: implementados elementos de pagina de perfil

--- src/presentation/pages/profile/index.tsx
+++ src/presentation/pages/profile/index.tsx
@@ -1,11 +1,146 @@
 import React from "react"
+import classNames from "classnames";
+import styles from './profile.module.scss';
+import {
+    Avatar,
+    Box,
+    createTheme,
+    Divider,
+    IconButton,
+    InputAdornment,
+    StandardTextFieldProps,
+    TextField,
+    ThemeProvider,
+    Typography
+} from "@mui/material";
+import { IconType } from "react-icons";
+import { FiEye, FiEyeOff, FiLock, FiMail, FiUser } from "react-icons/fi";
+import { useAuth } from "../../contexts/auth";
+import { purplePalette } from "../../styles/colors";
+import { usePassword } from "../../hooks/use-password";
 
 type ProfileProps = {}
 
 export const Profile = ( _: ProfileProps ) => {
+    const { user } = useAuth();
+    const [ isVisible, toggleVisibility, type ] = usePassword()
+
     return (
-        <>
-            Profile
-        </>
+        <div className={ classNames(styles.profileWrapper) }>
+            <Typography variant={ 'h5' }>
+                Perfil
+            </Typography>
+
+            <div className={ classNames(styles.contentWrapper) }>
+                <div className={ classNames(styles.profileCard) }>
+                    <div className={ styles.avatarBg }>
+                        <Avatar sx={ { width: 80, height: 80 } } src={ `${ process.env.REACT_APP_HOST }/avatar.jpg` }/>
+                    </div>
+
+                    <span>{ `${ user.name } ${ user.lastname }` }</span>
+
+                    <Box display={ 'block' } marginY={ 4 } width={ '90%' }>
+                        <Divider variant={ "fullWidth" }/>
+                    </Box>
+
+                    <ThemeProvider theme={ theme }>
+                        <Box display={ 'flex' } flexDirection={ 'column' } width={ '100%' } paddingX={ 5 } gap={ 2 }>
+                            <TextField
+                                placeholder={ "John doe" }
+                                value={ user.name }
+                                label={ "Nome" }
+                                fullWidth
+                                InputProps={ startAdornment(FiUser) }
+                            />
+
+                            <TextField
+                                placeholder={ "Due" }
+                                value={ user.lastname }
+                                label={ "Sobrenome" }
+                                fullWidth
+                                InputProps={ startAdornment(FiUser) }
+                            />
+
+                            <TextField
+                                placeholder={ "Due" }
+                                value={ user.email }
+                                label={ "E-mail" }
+                                fullWidth
+                                InputProps={ startAdornment(FiMail) }
+                            />
+
+                            <TextField
+                                placeholder={ "Sua#Senha@!Ultra!Secreta" }
+                                label={ "Senha" }
+                                type={ type }
+                                fullWidth
+                                InputProps={ {
+                                    ...startAdornment(FiLock),
+                                    ...passwordAdornment({
+                                        isVisible,
+                                        VisibleIcon: FiEyeOff,
+                                        HiddenIcon: FiEye,
+                                        onClick: toggleVisibility
+                                    })
+                                } }
+                            />
+                        </Box>
+                    </ThemeProvider>
+                </div>
+
+                <div className={ classNames(styles.reportCards) }>
+                    <div>
+                        <Typography variant={ 'h5' }>Suas Conquistas</Typography>
+                    </div>
+
+                    <div>
+                        <Typography variant={ 'h5' }>Suas Estatísticas</Typography>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
+
+const startAdornment = ( Icon: IconType ): StandardTextFieldProps['InputProps'] => ({
+    startAdornment: (
+        <InputAdornment position={ 'start' }>
+            <Icon size={ 24 }/>
+        </InputAdornment>
+    )
+})
+
+type PasswordArgs = {
+    isVisible: boolean;
+    VisibleIcon: IconType;
+    HiddenIcon: IconType;
+    onClick: () => void;
+}
+const passwordAdornment = (
+    {
+        isVisible,
+        VisibleIcon,
+        HiddenIcon,
+        onClick
+    }: PasswordArgs ): StandardTextFieldProps['InputProps'] => ({
+    endAdornment: (
+        <InputAdornment position={ 'end' }>
+            <IconButton onClick={ onClick }>
+                { isVisible
+                    ? <VisibleIcon size={ 24 }/>
+                    : <HiddenIcon size={ 24 }/>
+                }
+            </IconButton>
+        </InputAdornment>
+    )
+})
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            ...purplePalette,
+            main: (purplePalette as any)['300'],
+        }
+    }
+})
