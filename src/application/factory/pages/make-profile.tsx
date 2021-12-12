f695c52698fcdb3779ce0b014d8abd398657ced9
Jean <jean.carlosmolossi@gmail.com>
2021-12-11 22:53:15 -0300

    feat: criada página de perfil

---
+++ src/application/factory/pages/make-profile.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { Profile } from "../../../presentation/pages/profile";
+
+export const MakeProfile = () => {
+    return <Profile/>
+}
---
+++ src/presentation/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type ProfileProps = {}
+
+export const Profile = ( _: ProfileProps ) => {
+    return (
+        <>
+            Profile
+        </>
+    )
+}
